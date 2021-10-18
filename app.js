7ff79b03cf03ba841eaa8690bf099c20946cfc20
BrianATOlsen <90627608+BrianATOlsen@users.noreply.github.com>
2021-10-18 11:00:57 -0300

    Initialized
    Started

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+    Brian Olsen
+    5055815
+    JavaScript Programming
+    Exercise 7
+    October 18,2021
+ */
+
+const discountPrice = function (value, discountPercentage) { return value – (value * discountPercentage); }
